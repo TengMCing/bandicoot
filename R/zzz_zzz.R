3ab15d89bfe8d4f89d1177fb7d1eb81f54f2e5b7
TengMCing <llreczx@gmail.com>
2022-06-30 01:11:35 +1000

    Fix #8 base_() now correctly captures the call

--- R/zzz_zzz.R
+++ R/zzz_zzz.R
@@ -5,12 +5,14 @@
 #' @description These functions are the same as `class$instantiate`.
 #' @param ... Arguments passed to `init` method.
 #' @param env Environment. The instance environment.
+#' @param init_call Call. Contents of the `..init_call..`. It is recommended to
+#' leave it as default. Default is `init_call = sys.call()`.
 NULL
 
 #' @describeIn portal [BASE]
 #' @export
-base_ <- function(..., env = new.env(parent = parent.frame())) {
-  BASE$instantiate(..., env = env)
+base_ <- function(..., env = new.env(parent = parent.frame()), init_call = sys.call()) {
+  BASE$instantiate(..., env = env, init_call = init_call)
 }
 
 .onLoad <- function(libname, pkgname) {
